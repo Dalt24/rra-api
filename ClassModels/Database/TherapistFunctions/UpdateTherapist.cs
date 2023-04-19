f24d2de04165e40d411d462b5cc8e961b32e48bb
Dalt24 <dcwright4@crimson.ua.edu>
2023-04-18 23:24:46 -0500

    pre-security save

--- ClassModels/Database/TherapistFunctions/UpdateTherapist.cs
+++ ClassModels/Database/TherapistFunctions/UpdateTherapist.cs
@@ -12,7 +12,6 @@ namespace api_rra1.ClassModels.Database.TherapistFunctions
             if (isOpen)
             {
                 // string stm = @"INSERT INTO therapists(therapistID, firstName, lastName, availability, emailAddress, therapistPassword, isAdmin, isTherapist) VALUES(@therapistID, @firstName, @lastName, @availability, @emailAddress, @therapistPassword, @isAdmin, @isTherapist)";
-                Console.WriteLine(id);
                 string stm = @"UPDATE therapists SET availability=@availability, therapistPassword=@therapistPassword WHERE therapistID =@id";
                 MySqlConnection? con = db.GetCon();
                 using var cmd = new MySqlCommand(stm, con);
